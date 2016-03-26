27964e78bc0b389cd16d5f22045c0b5ad0911eed
えばたあや <ayaaaaa.0u0w@gmail.com>
2016-03-26 11:56:03 +0900

    saveTotalFeeのところメソッドチェーンにした

--- app/src/main/java/com/example/aya/nantokaapp/MainActivity.java
+++ app/src/main/java/com/example/aya/nantokaapp/MainActivity.java
@@ -50,11 +50,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void saveTotalFee(int totalFee) {
         SharedPreferences pref = getSharedPreferences(KEY_TOTAL_FEE, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
-        editor.putInt(KEY_TOTAL_FEE, totalFee).apply();
-        editor.apply();
-        // 好みだけどメソッドチェーンでも
-        //pref.edit().putInt(KEY_TOTAL_FEE, totalFee).apply();
+        pref.edit().putInt(KEY_TOTAL_FEE, totalFee).apply();
     }
 
     private int getTotalFee() {
